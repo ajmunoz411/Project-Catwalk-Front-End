16f58f6660a3bb3e0d249527ffc6e1fc86eb36b9
Andrew Munoz <ajmunoz411@gmail.com>
2021-09-13 18:47:00 -0700

    Re-ordered / grouped router sections

--- server/router.js
+++ server/router.js
@@ -1,38 +1,25 @@
 const router = require('express').Router();
 const atelier = require('../helpers/atelier');
 
-router.get('/products', atelier.getProducts);
+router
+  .get('/products', atelier.getProducts)
+  .get('/products/:id', atelier.getOneProduct)
+  .get('/products/:id/styles', atelier.getProductStyles)
+  .get('/related/:id', atelier.getRelatedItems)
 
-router.get('/qa/questions', atelier.getQuestions);
+  .get('/reviews2/:id/:count/:sort', atelier.getReviews)
+  .get('/reviews/meta/:id', atelier.getReviewMetadata)
+  .put('/reviews2/:id/helpful', atelier.updateHelpfulReview)
+  .put('/reviews2/:id/report', atelier.reportReview)
+  .post('/reviews2/postReview', atelier.postReview)
 
-router.get('/products/:id/styles', atelier.getProductStyles);
-
-router.get('/reviews2/:id/:count/:sort', atelier.getReviews);
-
-router.put('/reviews2/:id/helpful', atelier.updateHelpfulReview);
-
-router.put('/reviews2/:id/report', atelier.reportReview);
-
-router.post('/reviews2/postReview', atelier.postReview);
-
-router.get('/reviews/meta/:id', atelier.getReviewMetadata);
-
-router.get('/related/:id', atelier.getRelatedItems);
-
-router.get('/products/:id', atelier.getOneProduct);
-
-router.get('/qa/questions/:question_id/answers', atelier.getAnswersByQuestionId);
-
-router.put('/qa/answers/:answer_id/helpful', atelier.updateHelpfulAnswer);
-
-router.put('/qa/answers/:answer_id/report', atelier.reportAnswers);
-
-router.put('/qa/questions/:question_id/helpful', atelier.updateHelpfulQuestion);
-
-router.post('/qa/questions/:question_id/answers', atelier.postAnswer);
-
-router.post('/qa/questions', atelier.postQuestion);
-
-router.put('/qa/questions/:question_id/report', atelier.reportQuestion);
+  .get('/qa/questions', atelier.getQuestions)
+  .get('/qa/questions/:question_id/answers', atelier.getAnswersByQuestionId)
+  .put('/qa/questions/:question_id/helpful', atelier.updateHelpfulQuestion)
+  .put('/qa/questions/:question_id/report', atelier.reportQuestion)
+  .put('/qa/answers/:answer_id/helpful', atelier.updateHelpfulAnswer)
+  .put('/qa/answers/:answer_id/report', atelier.reportAnswers)
+  .post('/qa/questions/:question_id/answers', atelier.postAnswer)
+  .post('/qa/questions', atelier.postQuestion);
 
 module.exports = router;
